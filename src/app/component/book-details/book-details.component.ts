f60599b181328a03f13094a46ae213971a74ff83
safi <safiasad12@gmail.com>
2025-02-20 11:21:09 +0530

    [safi] refactor : code to conditionaly render the cart button in books details copmponent

--- src/app/component/book-details/book-details.component.ts
+++ src/app/component/book-details/book-details.component.ts
@@ -1,60 +1,79 @@
-  import { Component } from '@angular/core';
-  import { BookService } from 'src/app/service/book-service/book.service';
-  import { ActivatedRoute } from '@angular/router';
-  import { DataService } from 'src/app/service/data-service/data.service';
+import { Component } from '@angular/core';
+import { BookService } from 'src/app/service/book-service/book.service';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/service/data-service/data.service';
 
-  @Component({
-    selector: 'app-book-details',
-    templateUrl: './book-details.component.html',
-    styleUrls: ['./book-details.component.scss']
-  })
-  export class BookDetailsComponent {
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent {
+  
+  book: any = {};
+  bookId: string | null = null;
+  name: string = "";
+  author: string = "";
+  price: number = 0;
+  details: string = "";
+  image: string = "";
+  quantity: number = 1;
+  inWishlist: boolean = false;
+  outOfStock: boolean = false;
 
-    book: any ={}
-    bookId: string | null = null;
-    name: string = "";
-    author: string = "";
-    price: number = 0;
-    details: string = "";
-    image: string = "";
-    quantity: number = 1;
-    inCart: boolean = false;
-    inWishlist: boolean = false;
-    outOfStock: boolean = false;
-    
-    constructor(private bookService: BookService, private route: ActivatedRoute, private dataService: DataService) {}
+  constructor(
+    private bookService: BookService, 
+    private route: ActivatedRoute, 
+    private dataService: DataService
+  ) {}
 
-    ngOnInit() {
-      this.route.paramMap.subscribe(params => {
-        this.bookId = params.get('id');
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.bookId = params.get('id');
+    });
+
+    if (this.bookId) {
+      this.bookService.fetchBookByIdApiCall(this.bookId).subscribe({
+        next: (res: any) => {
+          this.name = res.data.bookName;
+          this.author = res.data.author;
+          this.price = res.data.price;
+          this.details = res.data.description;
+          this.image = res.data.bookImage;
+          this.outOfStock = res.data.quantity === 0;
+          this.book = res.data;
+          this.quantity = res.data.quantity;
+        },
+        error: (err) => {
+          console.log(err);
+        }
       });
-
-      if (this.bookId) {
-        this.bookService.fetchBookByIdApiCall(this.bookId).subscribe({
-          next: (res: any) => {
-            this.name = res.data.bookName;
-            this.author = res.data.author;
-            this.price = res.data.price;
-            this.details = res.data.description;
-            this.image = res.data.bookImage;
-            this.outOfStock = res.data.quantity === 0;
-            this.book = res.data;
-            this.quantity=res.quantity
-          },
-          error: (err) => {
-            console.log(err);
-          }
-        });
-      }
-    }
-
-    addToCart() {
-      this.inCart = true;
-      this.dataService.addToCart(this.book);    
-    }
-
-    addToWishlist(){
-      this.inWishlist = true;
-      this.dataService.addToWishlist(this.book);
     }
   }
+
+  addToCart() {
+    this.dataService.addToCart(this.book);    
+  }
+
+  addToWishlist() {
+    this.inWishlist = true;
+    this.dataService.addToWishlist(this.book);
+  }
+
+  inCart(): boolean {
+    if (!this.bookId) return false;
+    return this.dataService.getCartItems().some((item: any) => item._id === this.bookId);
+  }
+
+  getCartQuantity(): number {
+    if (!this.bookId) return 0;
+    const bookInCart = this.dataService.getCartItems().find((item: any) => item._id === this.bookId);
+    return bookInCart ? bookInCart.quantity : 0;
+  }
+
+  updateQuantity(change: number) {
+    if (this.bookId) {
+      this.dataService.updateQuantity(this.bookId, change);
+    }
+  }
+}
--- src/app/service/data-service/data.service.ts
+++ src/app/service/data-service/data.service.ts
@@ -6,24 +6,26 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class DataService {
 
-
   private cartItems = new BehaviorSubject<any[]>([]);
   cartItems$ = this.cartItems.asObservable();
 
   private wishlist = new BehaviorSubject<any[]>([]);
   wishlist$ = this.wishlist.asObservable();
 
+  getCartItems(): any[] {
+    return this.cartItems.getValue(); 
+  }
 
   addToCart(book: any) {
-    const currentCart = this.cartItems.getValue();
+    const currentCart = this.getCartItems();
     if (!currentCart.find(item => item._id === book._id)) {
-      currentCart.push({ ...book });
+      currentCart.push({ ...book, quantity: 1 }); 
       this.cartItems.next([...currentCart]);
     }
   }
 
   removeFromCart(bookId: string) {
-    const updatedCart = this.cartItems.getValue().filter(item => item._id !== bookId);
+    const updatedCart = this.getCartItems().filter(item => item._id !== bookId);
     this.cartItems.next(updatedCart);
   }
 
@@ -31,6 +33,16 @@ export class DataService {
     this.cartItems.next(updatedCart);
   }
 
+  updateQuantity(bookId: string, change: number) {
+    let updatedCart = this.getCartItems().map(item => {
+      if (item._id === bookId) {
+        return { ...item, quantity: item.quantity + change };
+      }
+      return item;
+    }).filter(item => item.quantity > 0); 
+
+    this.updateCart(updatedCart);
+  }
 
   addToWishlist(book: any) {
     const currentWishlist = this.wishlist.getValue();
@@ -40,12 +52,8 @@ export class DataService {
     }
   }
 
-  
-
   removeFromWishlist(bookId: string) {
     const updateWishlist = this.wishlist.getValue().filter(item => item._id !== bookId);
     this.wishlist.next(updateWishlist);
   }
 }
-
-
